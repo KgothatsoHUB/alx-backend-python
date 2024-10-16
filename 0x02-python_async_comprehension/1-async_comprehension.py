619b39bcb73d629db047f982673fe676a9e54fd2
Kgothatso H. Khoza <kgothikhoza@gmail.com>
2024-10-16 14:53:25 +0000

    0x01

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import asyncio
-from typing import List
-from 0-async_generator import async_generator
-
-async def async_comprehension() -> List[float]:
-    return [number async for number in async_generator()]

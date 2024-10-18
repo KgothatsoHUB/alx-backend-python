748f00ef24fc33e6d257c0e8f8481a38f7080f3a
Kgothatso H. Khoza <kgothikhoza@gmail.com>
2024-10-18 14:25:38 +0000

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,8 +1,9 @@
 import asyncio
 import time
 
-# Dynamically import the module
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+# Dynamically import the module with a name starting with a number
+module = __import__('1-async_comprehension')
+async_comprehension = getattr(module, '1-async_comprehension')
 
 async def measure_runtime() -> float:
     """Measures the total runtime for executing async_comprehension 4 times in parallel."""

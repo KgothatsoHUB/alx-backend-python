1eeb7f8155992b03bc663092a7da38d4c120a4fa
Kgothatso H. Khoza <kgothikhoza@gmail.com>
2024-10-18 14:52:39 +0000

    1-async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,14 +5,14 @@ Module to asynchronously collect float values from an async generator.
 
 import asyncio
 from typing import List
-from 0_async_generator import async_generator
+from 0_async_generator import async_generator  
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> List[float]:  # Proper return annotation
     """
-    Asynchronously collects 10 float values from an async generator.
+    Asynchronously collects float values from an async generator.
 
     Returns:
         List[float]: A list of collected float values.
     """
-    return [value async for value in async_generator()]
+    return [value async for value in async_generator()]  # Ensure float values are yielded
 
